aa6b1122501b58d28e1e9efd2ddf1d14c3c37a80
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2023-02-13 19:55:21 +0200

    create TodoList and TodoItem components

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useAppSelector} from '../hooks'
+import TodoItem from "./TodoItem";
 
 const TodoList: React.FC = () => {
+    const {todos} = useAppSelector(state => state.todos)
+
     return (
-        <div>
-            TodoList
+        <div className='todo-container'>
+            <ul className='todo-list'>
+                {todos.map((todo) => (
+                    <TodoItem
+                        key={todo.id}
+                        id={todo.id}
+                        title={todo.title}
+                        completed={todo.completed}
+                    />
+                ))}
+            </ul>
         </div>
     );
 };
